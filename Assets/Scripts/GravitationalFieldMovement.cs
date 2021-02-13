32f085bf0bf95785cb317849a933a8224dfaca25
Vlad <Vikhrov.VS@phystech.edu>
2021-02-13 17:26:02 +0300

    Heavy body position implemented

--- Assets/Scripts/GravitationalFieldMovement.cs
+++ Assets/Scripts/GravitationalFieldMovement.cs
@@ -8,20 +8,27 @@ public class GravitationalFieldMovement: MonoBehaviour
     [SerializeField] private float mass;
     [SerializeField] private Vector3 speed;
     [SerializeField] private Vector3 startingPosition;
+    [SerializeField] private GameObject heavyBody;
     [SerializeField] private float g;
+    private Vector3 _heavyBodyPosition = Vector3.zero;
     private const float DeltaTime = 0.02f; //default value
     private const float MaxAcceleration = 25;
+    
     private void Start()
     {
+        if (heavyBody)
+        {
+            _heavyBodyPosition = heavyBody.transform.position;
+        }
         Time.fixedDeltaTime = DeltaTime;
         transform.position = startingPosition;
     }
     
     private void FixedUpdate()
     {
-        float deltaTime = Time.deltaTime;
+        var deltaTime = Time.deltaTime;
         Vector3 position = transform.position;
-        Vector3 acceleration = -1 * g * mass * position / (float) Math.Pow(position.magnitude, 3);
+        Vector3 acceleration = -1 * g * mass * (position - _heavyBodyPosition) / (float) Math.Pow((position - _heavyBodyPosition).magnitude, 3);
         if (acceleration.magnitude > MaxAcceleration)
         {
             acceleration = acceleration.normalized * MaxAcceleration;
